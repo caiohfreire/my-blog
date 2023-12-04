946e4839ae03665dbf884e4646327facf7b1b6a6
Caio Freire <caiohenriquefreire@icloud.com>
2023-12-04 15:49:28 -0300

    Update axios.ts

--- src/service/axios.ts
+++ src/service/axios.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export const Axios = axios.create({
-  baseURL: 'http://localhost:8080',
+  // baseURL: 'http://localhost:8080',
+  baseURL: 'https://blog-api-5l0l.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   }
