03bdf5a7a409c08dd77d49eec4e9e6bec5ff83c0
Caio Freire <caiohenriquefreire@icloud.com>
2023-12-04 12:07:59 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/', // Certifique-se de ajustar conforme necessário ('/' se estiver na raiz, '/seu-app/' se estiver em um subdiretório)
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3000', // Se estiver usando uma API, configure um proxy
+    },
+  },
+  build: {
+    outDir: 'dist', // Certifique-se de que corresponde ao seu diretório de saída
+  },
+});
