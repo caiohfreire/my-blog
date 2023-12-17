1afbaa766cbd753f5e26af771908fa615c0ad6ee
Caio Freire <caiohenriquefreire@icloud.com>
2023-12-16 23:58:21 -0300

    .

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -12,7 +12,9 @@ export default function Navbar() {
     <div className='block'>
       <nav className="fixed top-0 z-50 flex w-full h-20 bg-neutral-950 dark:bg-stone-200 shadow-xl">
         <div className="max-w-[1280px] flex justify-between px-4 w-full items-center mx-auto">
-          <h2 className="font-bold text-2xl text-white dark:text-stone-800 cursor-pointer" onClick={() => router('/')}>MyBlog</h2>
+          <h2 className="font-bold text-2xl tracking-wider text-white dark:text-stone-800 cursor-pointer" onClick={() => router('/')}>
+            FORCE
+          </h2>
           <div className='flex items-center gap-4'>
             {isAuthenticated && (
               <ul className='flex items-center gap-8'>
